a5d2a8aa7bbac36cf271d026278c9df429fe982a
zui <zui.develop0000@gmail.com>
2026-03-26 00:01:00 +0900

    fix: 相対パスを修正

--- apps/baseui-frontend/tailwind.config.ts
+++ apps/baseui-frontend/tailwind.config.ts
@@ -2,7 +2,7 @@ import { colors, zIndex } from "@packages/ui/styles";
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{ts,tsx}", "../../packages/ui/src/**/*.{ts,tsx}"],
   theme: {
     extend: {
       colors,
