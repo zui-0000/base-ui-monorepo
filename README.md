1657bbea198940ef9f472d384f5305e22e1cd5d7
zui <zui.develop0000@gmail.com>
2026-05-11 00:27:22 +0900

    chore: バージョンアップ+設定追加

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
 # base-ui-monorepo
 
-## miseセットアップ
-
 ```sh
 mise install
 ```
 
-## 起動
-
 ```sh
 pnpm install
-pnpm dev
-
-pnpm lint:fix
+pnpm run dev:frontend
 ```
--- apps/baseui-frontend/vite.config.ts
+++ apps/baseui-frontend/vite.config.ts
@@ -3,14 +3,17 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  define: {
+    // Import.meta.env.VITE_APP_ENV の代わりに __APP_ENV__ を使用する
+    __APP_ENV__: JSON.stringify(mode),
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@packages/ui": resolve(__dirname, "../../packages/ui/src"),
       "@ui": resolve(__dirname, "../../packages/ui/src"),
-      "@": resolve(__dirname, "./src"),
       "~": resolve(__dirname, "./src"),
     },
   },
-});
+}));
